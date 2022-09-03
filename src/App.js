8dd1217a6d7eee54d83462981afa7658fe5116d2
hamdegithub <hetcode466@gmail.com>
2022-09-03 14:23:53 +0300

    Apple react
    apple

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "./Resource/css/styles.css";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import Alert from "./Components/Main/Alert/Alert";
+import Nav from "./Components/Nav/Nav";
+import First from "./Components/Main/First/First";
+import Second from "./Components/Main/Second/Second";
+import Third from "./Components/Main/Third/Third";
+import Fourth from "./Components/Main/Fourth/Fourth";
+import Fifth from "./Components/Main/Fifth/Fifth";
+import Sixth from "./Components/Main/Sixth/Sixth";
+import Footer from "./Components/Footer/Footer";
+import Move from "./Components/Main/Move/Move";
+// Project Console: https://console.firebase.google.com/project/apple-clone-e2be1/overview
+// Hosting URL: https://apple-clone-e2be1.web.app
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav></Nav>
+      <Alert></Alert>
+      <First></First>
+      <Second></Second>
+      <Third></Third>
+      <Fourth></Fourth>
+      <Fifth></Fifth>
+      <Sixth></Sixth>
+      <Move></Move>
+      <Footer></Footer>
     </div>
   );
 }
